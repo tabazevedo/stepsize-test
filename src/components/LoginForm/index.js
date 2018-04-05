ad7fcfbb9f6fd1a7d0a01608a41d4a27a1be485f
Tiago Azevedo <tabazevedo@gmail.com>
2018-04-05 14:32:00 +0100

    Add form labels

--- src/components/LoginForm/index.js
+++ src/components/LoginForm/index.js
@@ -3,12 +3,15 @@ import { connect } from 'react-redux';
 
 const LoginForm = ({ endpoint, login }) => (
   <form action={endpoint} method="post">
-    <input type="text" name="username"></input>
+    <label for="email">Email</label>
+    <input type="email" name="email"></input>
+    <label for="password">Password</label>
     <input type="password" name="password"></input>
     <input type="submit"></input>
   </form>
 );
 
+// TODO: Move this action into a reducer and handle async data fetch/route change
 const loginAction = (username, password) => ({ username, password });
 
 export default connect(
