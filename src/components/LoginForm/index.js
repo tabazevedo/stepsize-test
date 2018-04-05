6db482c5429c7c8306fb2438eb05d7612eea7261
Tiago Azevedo <tabazevedo@gmail.com>
2018-04-05 15:22:47 +0100

    Add event handlers for login form

--- src/components/LoginForm/index.js
+++ src/components/LoginForm/index.js
@@ -1,15 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { userLogin } from '../../redux/actions/user';
 
-const LoginForm = ({ login }) => (
-  <form onSubmit={login}>
-    <label htmlFor="email">Email</label>
-    <input type="email" name="email"></input>
-    <label htmlFor="password">Password</label>
-    <input type="password" name="password"></input>
-    <input type="submit"></input>
-  </form>
-);
+class LoginForm extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: '',
+      password: ''
+    }
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    const name = event.target.name;
+
+    if (name === 'email' || name === 'password') {
+      this.setState({ [name]: event.target.value });
+    }
+  }
+
+  handleSubmit() {
+    this.props.login(this.state.email, this.state.password)
+  }
+
+  render() {
+    return (
+      <form>
+        <label htmlFor="email">Email</label>
+        <input type="email" name="email" value={this.state.email} onChange={this.handleChange} />
+        <label htmlFor="password">Password</label>
+        <input type="password" name="password" value={this.state.password} onChange={this.handleChange}/>
+        <input type="submit" onClick={this.handleSubmit}/>
+      </form>
+    )
+  }
+}
 
 export default connect(null, { login: userLogin })(LoginForm);
